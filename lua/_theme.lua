e38a543ab8c4ff44191ab45f0bf92ac34c246cf4
Casey Webster <casey.webster@gmail.com>
2021-02-09 11:41:17 -0600

    Better setting of colo overrides

--- lua/_theme.lua
+++ lua/_theme.lua
@@ -1,48 +1,69 @@
 vim.g.jellybeans_overrides = {
   background = {
     ctermbg = 'none',
-    ["256ctermbg"] = 'none',
     guibg = 'none'
-  },
-  VertSplit = {
-    ctermbg = 'none',
-    guibg = 'none',
-    ["256ctermbg"] = 'none',
   },
 }
 
+vim.api.nvim_command [[syntax on]]
 vim.o.background = "dark"
 vim.g.jellybeans_use_term_italics = 1
 
 vim.api.nvim_command("colorscheme jellybeans")
+--vim.api.nvim_command("colorscheme zephyr")
 
 vim.fn.sign_define("LspDiagnosticsSignError", {text = "✘", texthl = "ALEErrorSign"})
 vim.fn.sign_define("LspDiagnosticsSignWarning", {text = "⚠", texthl = "ALEWarningSign"})
 vim.fn.sign_define("LspDiagnosticsSignInformation", {text = "🛈", texthl = "ALEInfoSign"})
 vim.fn.sign_define("LspDiagnosticsSignHint", {text = "💡", texthl = "ALEHintSign"})
 
-vim.api.nvim_exec([[
-augroup colors
-autocmd ColorScheme * highlight PMenu ctermbg=Black guibg=#191919
-autocmd ColorScheme * highlight PMenuSel guifg=#ffffff guibg=#333333
-autocmd ColorScheme * highlight clear SignColumn
-autocmd ColorScheme * highlight GitGutterAdd ctermbg=NONE ctermfg=green guibg=NONE guifg=green
-autocmd ColorScheme * highlight GitGutterChange ctermbg=NONE ctermfg=green guibg=NONE guifg=#2B5B77
-autocmd ColorScheme * highlight GitGutterDelete ctermbg=NONE ctermfg=red guibg=NONE guifg=red
-autocmd ColorScheme * highlight GitGutterChangeDelete ctermbg=NONE ctermfg=red guibg=NONE guifg=#2B5B77
-autocmd ColorScheme * highlight clear SpellCap
-autocmd ColorScheme * highlight clear SpellBad
-autocmd ColorScheme * highlight SpellCap guibg=NONE guisp='Red' gui=undercurl cterm=undercurl,bold
-autocmd ColorScheme * highlight SpellBad guibg=NONE guisp='Red' gui=undercurl guibg=#771c13 cterm=undercurl,bold
-autocmd ColorScheme * highlight ALEErrorSign ctermbg=NONE ctermfg=red guibg=NONE guifg=red
-autocmd ColorScheme * highlight ALEWarningSign ctermbg=NONE ctermfg=yellow guibg=NONE guifg=yellow
-autocmd ColorScheme * highlight ALEInfoSign ctermbg=NONE ctermfg=blue guibg=NONE guifg=blue
-autocmd ColorScheme * highlight ALEHintSign ctermbg=NONE ctermfg=green guibg=NONE guifg=green
-autocmd ColorScheme * highlight TSCurrentScope ctermbg=NONE guibg=NONE
-"autocmd ColorScheme * highlight NvimTreeFolderIcon guibg=blue
-autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline
-autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline
-augroup END
-]],
-false
-)
+-- my overrides over any ColorScheme
+local my_clear_colors = {'SignColumn', 'SpellBad', 'SpellCap'}
+local my_colors = {
+  --Normal =                {ctermbg='NONE',                  guibg='NONE'},
+  VertSplit =             {ctermbg='NONE',                  guibg='NONE'},
+  PMenu =                 {ctermbg='Black',                 guibg='#191919'},
+  PMenuSel =              {                guifg='#ffffff', guibg='#333333'},
+  GitGutterAdd =          {ctermbg='NONE', ctermfg='green', guibg='NONE', guifg='green'},
+  GitGutterChange =       {ctermbg='NONE', ctermfg='green', guibg='NONE', guifg='#2B5B77'},
+  GitGutterDelete =       {ctermbg='NONE', ctermfg='red',   guibg='NONE', guifg='red'},
+  GitGutterChangeDelete = {ctermbg='NONE', ctermfg='red',   guibg='NONE', guifg='#2B5B77'},
+  SpellCap =              {guibg='NONE', guisp='Red', gui='undercurl', cterm='undercurl,bold'},
+  SpellBad =              {guisp='Red', gui='undercurl', guibg='#771c13', cterm='undercurl,bold'},
+  ALEErrorSign =          {ctermbg='NONE', ctermfg='red',   guibg='NONE', guifg='red'},
+  ALEWarningSign =        {ctermbg='NONE', ctermfg='yellow', guibg='NONE', guifg='yellow'},
+  ALEInfoSign =           {ctermbg='NONE', ctermfg='blue',  guibg='NONE', guifg='blue'},
+  ALEHintSign =           {ctermbg='NONE', ctermfg='green', guibg='NONE', guifg='green'},
+  TSCurrentScope =        {ctermbg='NONE',                  guibg='NONE'},
+  QuickScopePrimary =     {guifg='#afff5f', gui='underline'},
+  QuickScopeSecondary =   {guifg='#5fffff', gui='underline'},
+}
+
+-- this sets colors and sets up an autocmd to set them again if colo is changed
+local mycolors = function(clear_colors, set_colors)
+  local highlights = {}
+
+  for _, v in pairs(clear_colors) do
+    table.insert(highlights, "highlight clear "..v)
+  end
+  for k,v in pairs(set_colors) do
+    local highlight = "highlight "..k
+    for attr, value in pairs(v) do
+      highlight = highlight.." "..attr.."="..value
+    end
+    table.insert(highlights, highlight)
+  end
+
+  for _, c in pairs(highlights) do
+    vim.api.nvim_command(c)
+  end
+  vim.api.nvim_command("augroup mycolors")
+  for _, c in pairs(highlights) do
+    vim.api.nvim_command("autocmd ColorScheme * "..c)
+  end
+  vim.api.nvim_command("augroup END")
+end
+
+mycolors(my_clear_colors, my_colors)
+
+--"autocmd ColorScheme * highlight NvimTreeFolderIcon guibg=blue
