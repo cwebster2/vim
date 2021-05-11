3c589f59a84be6842c6cf596a95ba8efe2b3a69f
Casey Webster <casey.webster@gmail.com>
2021-05-10 23:03:59 -0500

    dont let plugin bind to leader s

--- lua/plugin_config.lua
+++ lua/plugin_config.lua
@@ -33,6 +33,7 @@ g.better_whitespace_guicolor='red'
 g.better_whitespace_enabled=1
 g.strip_whitespace_on_save=1
 g.strip_whitespace_confirm=0
+g.better_whitespace_operator='_s'
 
 g.vim_markdown_folding_disabled = 1
 g.vim_markdown_conceal = 0
