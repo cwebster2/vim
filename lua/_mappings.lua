1877e197412c9471a68d67718fae17a9637ae552
Casey Webster <casey.webster@gmail.com>
2021-05-16 00:11:26 -0500

    Refactoring

--- lua/_mappings.lua
+++ lua/_mappings.lua
@@ -6,84 +6,89 @@ map('v', '<', '<gv', {noremap=true})
 map('v', '>', '>gv', {noremap=true})
 map('v', '.', ':normal .<CR>', {noremap=true})
 
-wk.register({
-    ["g"] = {
-      name = "+git",
-      ["g"] = { ":vertical Gstatus<cr>", "Fugitive" },
-      ["w"] = { ":Gwrite<cr>", "Git Write" },
-      --["c"] = { ":Git commit<cr>", "Commit" },
-      --["p"] = { ":Git push", "Push" },
-      --l = { function() require"util".float_terminal("lazygit") end, "LazyGit" },
-      ["c"] = { "<Cmd>Telescope git_commits<CR>", "commits" },
-      ["b"] = { "<Cmd>Telescope git_branches<CR>", "branches" },
-      ["s"] = { "<Cmd>Telescope git_status<CR>", "status" },
-      --["d"] = { "<cmd>DiffviewOpen<cr>", "DiffView" },
-      --h = { name = "+hunk" }
-      ["i"] = { "<cmd>lua require('telescope').extensions.gh.issues()<CR>", "issues" },
-      ["p"] = { "<cmd>lua require('telescope').extensions.gh.pull_request()<CR>", "pull requests" },
-    },
-    ["b"] = {
-      name = "+buffer",
-      ["g"] = { "<cmd>BufferPick<cr>", "Goto Buffer" },
-      ["p"] = { "<Cmd>BufferPrevious<CR>", "Previous Buffer" },
-      ["n"] = { "<Cmd>BufferNext<CR>", "Next Buffer" },
-      ["s"] = { "<cmd>lua require('telescope.builtin').buffers()<CR>", "Buffers" },
-      ["b"] = { ':e#<CR>', "Last Buffer"},
-      ["d"] = { '<Cmd>BufferOrderByDirectory<CR>', "Order by Dir" },
-      ["l"] = { '<Cmd>BufferOrderByLanguage<CR>', "Order by Lang" },
-    },
-    ["h"] = {
-      name = "+help",
-      ["t"] = { "<cmd>Telescope builtin<cr>", "Telescope" },
-      ["c"] = { "<cmd>Telescope commands<cr>", "Commands" },
-      ["h"] = { "<cmd>Telescope help_tags<cr>", "Help Pages" },
-      ["m"] = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-      ["k"] = { "<cmd>Telescope keymaps<cr>", "Key Maps" },
-      ["s"] = { "<cmd>Telescope highlights<cr>", "Search Highlight Groups" },
-      --["l = { [[<cmd>TSHighlightCapturesUnderCursor<cr>]], "Highlight Groups at cursor" },
-      ["f"] = { "<cmd>Telescope filetypes<cr>", "File Types" },
-      ["o"] = { "<cmd>Telescope vim_options<cr>", "Options" },
-      ["a"] = { "<cmd>Telescope autocommands<cr>", "Auto Commands" },
-      ["p"] = {
-        name = "+packer",
-        ["p"] = { "<cmd>PackerSync<cr>", "Sync" },
-        ["s"] = { "<cmd>PackerStatus<cr>", "Status" },
-        ["i"] = { "<cmd>PackerInstall<cr>", "Install" },
-        ["c"] = { "<cmd>PackerCompile<cr>", "Compile" }
-      }
-    },
-    ["f"] = {
-      name = "+file",
-      ["t"] = { "<cmd>NvimTreeToggle<cr>", "NvimTree" },
-      ["f"] = { "<cmd>lua require('telescope.builtin').find_files()<CR>", "Find Files" },
-      ["g"] = { "<cmd>lua require('telescope.builtin').git_files()<CR>", "Find Git Files" },
-      ["r"] = { "<cmd>lua require('telescope.builtin').oldfiles()<CR>", "Recent Files" },
-      ["n"] = { "<cmd>enew<CR>", "New File" },
-    },
-    ["s"] = {
-      name = "+search",
-      ["g"] = { "<cmd>lua require('telescope.builtin').live_grep()<CR>", "Grep" },
-      ["c"] = { '<cmd>let @/=""<cr>', "Clear search highlight" },
-      ["l"] = { "<cmd>lua require('telescope.builtin').loclist()<CR>", "Loclist" },
-      ["q"] = { "<cmd>lua require('telescope.builtin').quickfix()<CR>","Quickfix" },
-      ["s"] = { "<cmd>lua require('telescope.builtin').spell_suggest()<CR>", "Spelling" },
-    },
-    ["<C-s>"] = { "<Cmd>BufferPick<CR>", "Pick Buffer" },
-    ["m"] = { "<Cmd>BufferPrevious<CR>", "Previous Buffer" },
-    ["i"] = { "<Cmd>BufferNext<CR>", "Next Buffer" },
-  }, { prefix = "<leader>" })
+local leader_map = {
+  ["g"] = {
+    name = "+git",
+    ["g"] = { ":vertical Gstatus<cr>", "Fugitive" },
+    ["w"] = { ":Gwrite<cr>", "Git Write" },
+    --["c"] = { ":Git commit<cr>", "Commit" },
+    --["p"] = { ":Git push", "Push" },
+    --l = { function() require"util".float_terminal("lazygit") end, "LazyGit" },
+    ["c"] = { "<Cmd>Telescope git_commits<CR>", "commits" },
+    ["b"] = { "<Cmd>Telescope git_branches<CR>", "branches" },
+    ["s"] = { "<Cmd>Telescope git_status<CR>", "status" },
+    --["d"] = { "<cmd>DiffviewOpen<cr>", "DiffView" },
+    --h = { name = "+hunk" }
+    ["i"] = { "<cmd>lua require('telescope').extensions.gh.issues()<CR>", "issues" },
+    ["p"] = { "<cmd>lua require('telescope').extensions.gh.pull_request()<CR>", "pull requests" },
+  },
+  ["b"] = {
+    name = "+buffer",
+    ["g"] = { "<cmd>BufferPick<cr>", "Goto Buffer" },
+    ["p"] = { "<Cmd>BufferPrevious<CR>", "Previous Buffer" },
+    ["n"] = { "<Cmd>BufferNext<CR>", "Next Buffer" },
+    ["s"] = { "<cmd>lua require('telescope.builtin').buffers()<CR>", "Buffers" },
+    ["b"] = { ':e#<CR>', "Last Buffer"},
+    ["d"] = { '<Cmd>BufferOrderByDirectory<CR>', "Order by Dir" },
+    ["l"] = { '<Cmd>BufferOrderByLanguage<CR>', "Order by Lang" },
+  },
+  ["h"] = {
+    name = "+help",
+    ["t"] = { "<cmd>Telescope builtin<cr>", "Telescope" },
+    ["c"] = { "<cmd>Telescope commands<cr>", "Commands" },
+    ["h"] = { "<cmd>Telescope help_tags<cr>", "Help Pages" },
+    ["m"] = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+    ["k"] = { "<cmd>Telescope keymaps<cr>", "Key Maps" },
+    ["s"] = { "<cmd>Telescope highlights<cr>", "Search Highlight Groups" },
+    --["l = { [[<cmd>TSHighlightCapturesUnderCursor<cr>]], "Highlight Groups at cursor" },
+    ["f"] = { "<cmd>Telescope filetypes<cr>", "File Types" },
+    ["o"] = { "<cmd>Telescope vim_options<cr>", "Options" },
+    ["a"] = { "<cmd>Telescope autocommands<cr>", "Auto Commands" },
+    ["p"] = {
+      name = "+packer",
+      ["p"] = { "<cmd>PackerSync<cr>", "Sync" },
+      ["s"] = { "<cmd>PackerStatus<cr>", "Status" },
+      ["i"] = { "<cmd>PackerInstall<cr>", "Install" },
+      ["c"] = { "<cmd>PackerCompile<cr>", "Compile" }
+    }
+  },
+  ["f"] = {
+    name = "+file",
+    ["t"] = { "<cmd>NvimTreeToggle<cr>", "NvimTree" },
+    ["f"] = { "<cmd>lua require('telescope.builtin').find_files()<CR>", "Find Files" },
+    ["g"] = { "<cmd>lua require('telescope.builtin').git_files()<CR>", "Find Git Files" },
+    ["r"] = { "<cmd>lua require('telescope.builtin').oldfiles()<CR>", "Recent Files" },
+    ["n"] = { "<cmd>enew<CR>", "New File" },
+  },
+  ["s"] = {
+    name = "+search",
+    ["g"] = { "<cmd>lua require('telescope.builtin').live_grep()<CR>", "Grep" },
+    ["c"] = { '<cmd>let @/=""<cr>', "Clear search highlight" },
+    ["l"] = { "<cmd>lua require('telescope.builtin').loclist()<CR>", "Loclist" },
+    ["q"] = { "<cmd>lua require('telescope.builtin').quickfix()<CR>","Quickfix" },
+    ["s"] = { "<cmd>lua require('telescope.builtin').spell_suggest()<CR>", "Spelling" },
+  },
+  ["m"] = { "<Cmd>BufferPrevious<CR>", "Previous Buffer" },
+  ["i"] = { "<Cmd>BufferNext<CR>", "Next Buffer" },
+}
 
-wk.register({
+local normal_map = {
   ["g"] = {
     name = "+goto",
     ["c"] = { "Comment" },
   },
-  ["<C-_>"] = { "<cmd>CommentToggle<cr>", "Comment Line" }
-}, {})
+  ["<C-_>"] = { "<cmd>CommentToggle<cr>", "Comment Line" },
+  ["<C-s>"] = { "<Cmd>BufferPick<CR>", "Pick Buffer" },
+  ["<C-p>"] = { "<Cmd>FGFiles<CR>", "Git Files" },
+}
 
-wk.register({
-  ["<C-_>"] = { ":'<,'>CommentToggle<cr>", "Comment Line" }
-}, { mode = "v" })
+local visual_map = {
+  ["<C-_>"] = { ":'<,'>CommentToggle<cr>", "Comment Lines" }
+}
+
+wk.register(leader_map, { prefix = "<leader>" })
+wk.register(normal_map, {})
+wk.register(visual_map, { mode = "v" })
 
 --map('n', '<Leader>fo', "<cmd>lua require('telescope.builtin').colorscheme()<CR>", {noremap=true})
 
