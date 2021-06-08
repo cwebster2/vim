b45db8966ec22fdd697ca17589bc5a2845343889
Casey Webster <cwebster@glgroup.com>
2021-06-08 12:52:31 -0500

    No need to confirm

--- lua/_neogit.lua
+++ lua/_neogit.lua
@@ -5,6 +5,7 @@ function M.setup()
   neogit.setup({
     disable_signs = false,
     disable_context_highlighting = false,
+    disable_commit_confirmation = true,
     signs = {
       -- { CLOSED, OPENED }
       section = { ">", "v" },
