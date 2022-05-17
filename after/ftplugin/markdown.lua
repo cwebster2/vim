6b556898b14737f5e00c4d2f633a803bd826eaab
Casey Webster <cwebster@glgroup.com>
2022-05-17 15:41:50 -0500

    Refactoring the ftplugin

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,14 +1,14 @@
-vim.api.nvim_win_set_option(0, "foldmethod", "expr")
-vim.api.nvim_buf_set_option(0, "textwidth", 80)
-vim.api.nvim_win_set_option(0, "colorcolumn", "81")
-vim.api.nvim_buf_set_option(0, "wrapmargin", 0)
-vim.api.nvim_win_set_option(0, "spell", true)
 
+vim.opt_local.foldmethod = "expr"
+vim.opt_local.textwidth = 80
+vim.opt_local.colorcolumn = "81"
+vim.opt_local.wrapmargin = 0
+vim.opt_local.spell = true
+
+-- vim.opt_local.formatoptions = vim.opt.formatoptions -"t"
+vim.opt_local.conceallevel = 0
 vim.cmd[[
-  setlocal fo-=t
-
   " Disable hidden char due to indentLine plugin
-  setlocal conceallevel=0
   let g:indentLine_concealcursor = 'invc'
   let g:indentLine_conceallevel = 2
 ]]
