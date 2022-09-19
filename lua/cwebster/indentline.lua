dfaafe49b00994a9ee8a4e41c286639199fc70a8
Casey Webster <cwebster@glgroup.com>
2022-09-19 17:09:06 -0500

    Changing some indentline options

--- lua/cwebster/indentline.lua
+++ lua/cwebster/indentline.lua
@@ -34,6 +34,22 @@ function M.setup()
     "object",
     "dictionary"
   }
+
+  require("indent_blankline").setup({
+    -- show_end_of_line = true,
+    space_char_blankline = " ",
+    show_current_context = true,
+    -- show_current_context_start = true,
+    show_trailing_blankline_indent = true,
+    -- char_highlight_list = {
+    --     "IndentBlanklineIndent1",
+    --     "IndentBlanklineIndent2",
+    --     "IndentBlanklineIndent3",
+    --     "IndentBlanklineIndent4",
+    --     "IndentBlanklineIndent5",
+    --     "IndentBlanklineIndent6",
+    -- },
+  })
 end
 
 return M
