dd5ee89fe20034ca2aef1255e7398940af6903c6
Casey Webster <cwebster@glgroup.com>
2022-09-21 16:47:18 -0500

    Recolor vertsplit

--- lua/cwebster/colors/catppuccin.lua
+++ lua/cwebster/colors/catppuccin.lua
@@ -12,6 +12,7 @@ M.overrides = {
 		  PmenuSel = { fg = mocha.sapphire },
 		  PmenuSbar = { bg = mocha.base }, -- Popup menu: scrollbar.
 		  PmenuThumb = { bg = mocha.surface0 }, -- Popup menu: Thumb of the scrollbar.
+      VertSplit = { fg = mocha.surface0 },
     }
     -- mocha = {
     --   ["@field"] = { fg = mocha.rosewater },
