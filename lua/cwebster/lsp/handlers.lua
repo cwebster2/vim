05ad25d9029027572867e540cf226684adb2e896
Casey Webster <cwebster@glgroup.com>
2022-11-01 10:01:51 -0500

    Dont set showMessage handler

--- lua/cwebster/lsp/handlers.lua
+++ lua/cwebster/lsp/handlers.lua
@@ -89,15 +89,15 @@ function M.setup()
   --   end
   -- end
 
-  local severity = {
-    "error",
-    "warn",
-    "info",
-    "info", -- map both hint and info to info?
-  }
-  vim.lsp.handlers["window/showMessage"] = function(err, method, params, client_id)
-    vim.notify(method.message, severity[params.type])
-  end
+  -- local severity = {
+  --   "error",
+  --   "warn",
+  --   "info",
+  --   "info", -- map both hint and info to info?
+  -- }
+  -- vim.lsp.handlers["window/showMessage"] = function(err, method, params, client_id)
+  --   vim.notify(method.message, severity[params.type])
+  -- end
 
 end
 
