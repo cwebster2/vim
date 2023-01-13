cfbb6dffe4f5688a2efe0199d305375bd429e749
Casey Webster <cwebster@glgroup.com>
2023-01-13 13:36:11 -0600

    Refactor

--- lua/cwebster/plugins/editor.lua
+++ lua/cwebster/plugins/editor.lua
@@ -339,8 +339,29 @@ return {
 	},
 	{
 		"mfussenegger/nvim-lint",
-		config = function()
-			require("cwebster.diagnostics.nvimlint").setup()
+		opts = {
+			text = { "vale" },
+			markdown = { "vale" },
+			rst = { "vale" },
+			dockerfile = { "hadolint" },
+			javascript = { "eslint" },
+			typescript = { "eslint" },
+			vue = { "eslint" },
+			svelte = { "eslint" },
+			json = { "eslint" },
+			yaml = { "eslint" },
+			toml = { "eslint" },
+			sh = { "shellcheck" },
+			zsh = { "shellcheck" },
+			bash = { "shellcheck" },
+		},
+		config = function(plugin)
+			require("lint").linters_by_ft = plugin.opts
+			vim.api.nvim_create_autocmd("BufWritePost", {
+				callback = function()
+					require("lint").try_lint()
+				end,
+			})
 		end,
 	},
 	{
