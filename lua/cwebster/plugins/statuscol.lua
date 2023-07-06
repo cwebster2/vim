37453b47a45326a366c6caaf590db2806a1afbe1
Casey Webster <cwebster@glgroup.com>
2023-07-06 17:13:38 -0500

    working on statuscol

--- lua/cwebster/plugins/statuscol.lua
+++ lua/cwebster/plugins/statuscol.lua
@@ -1,8 +1,30 @@
+
 return {
   {
     "luukvbaal/statuscol.nvim",
-    opts = {
-
-    }
+    config = function(opts)
+      local builtin = require("statuscol.builtin")
+      require("statuscol").setup({
+        segments = {
+          { text = { "%C" }, click = "v:lua.ScFa" },
+          { text = { "%s" }, click = "v:lua.ScSa" },
+          {
+            text = { builtin.lnumfunc, " " },
+            condition = { true, builtin.not_empty },
+            click = "v:lua.ScLa",
+          },
+          -- { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+          -- {
+          --   sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+          --   click = "v:lua.ScSa"
+          -- },
+          -- { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+          -- {
+          --   sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+          --   click = "v:lua.ScSa"
+          -- },
+        },
+      })
+    end,
   }
 }
