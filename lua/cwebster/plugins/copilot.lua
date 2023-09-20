27c568a1f1a3d51ae5b5f2dd8449f18cdf811718
Casey Webster <cwebster@glgroup.com>
2023-09-20 13:34:34 -0500

    Update copilot config

--- lua/cwebster/plugins/copilot.lua
+++ lua/cwebster/plugins/copilot.lua
@@ -1,33 +1,17 @@
-local node = os.getenv("HOME") .. "/.local/share/fnm/node-versions/v16.15.1/installation/bin/node"
-
 return {
-	-- PLUGINS: copilot
-	-- { "github/copilot.vim",
-	--   init = "require('cwebster.copilot').setup()"
-	-- vim.g.copilot_no_tab_map = true
-	-- vim.g.copilot_assume_mapped = true
-	-- vim.g.copilot_tab_fallback = ""
-	-- vim.g.copilot_node_command = node
-	-- }
-	{
-		"zbirenbaum/copilot.lua",
-		event = { "VimEnter" },
-		opts = {
-			ft_disable = { "markdown" },
-			copilot_node_command = node,
-		},
-		config = function(plugin)
-			vim.defer_fn(function()
-				require("copilot").setup(plugin.opts)
-			end, 100)
-		end,
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-		module = "copilot_cmp",
-		dependencies = { "copilot.lua" },
-		opts = {
-			method = "getCompletionsCycling",
-		},
-	},
+  {
+    "zbirenbaum/copilot.lua",
+    event = { "InsertEnter" },
+    opts = {
+      filetypes = { markdown = false },
+    },
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    module = "copilot_cmp",
+    dependencies = { "copilot.lua" },
+    opts = {
+      method = "getCompletionsCycling",
+    },
+  },
 }
