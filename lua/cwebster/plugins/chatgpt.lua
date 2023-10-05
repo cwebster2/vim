07f049a0d7cfffcfdbb781d0e0c6dfb85f86790d
Casey Webster <cwebster@glgroup.com>
2023-10-05 15:44:08 -0500

    Add mappings for chatgpt

--- lua/cwebster/plugins/chatgpt.lua
+++ lua/cwebster/plugins/chatgpt.lua
@@ -6,6 +6,21 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim"
   },
+  keys = {
+    { "<leader>cgc", "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
+    { "<leader>cge", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "Edit with instruction", mode = { "n", "v" } },
+    { "<leader>cgg", "<cmd>ChatGPTRun grammar_correction<CR>", desc = "Grammar Correction", mode = { "n", "v" } },
+    { "<leader>cgt", "<cmd>ChatGPTRun translate<CR>", desc = "Translate", mode = { "n", "v" } },
+    { "<leader>cgk", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords", mode = { "n", "v" } },
+    { "<leader>cgd", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring", mode = { "n", "v" } },
+    { "<leader>cga", "<cmd>ChatGPTRun add_tests<CR>", desc ="Add Tests", mode = { "n", "v" } },
+    { "<leader>cgo", "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code", mode = { "n", "v" } },
+    { "<leader>cgs", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize", mode = { "n", "v" } },
+    { "<leader>cgf", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs", mode = { "n", "v" } },
+    { "<leader>cgx", "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code", mode = { "n", "v" } },
+    { "<leader>cgr", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit", mode = { "n", "v" } },
+    { "<leader>cgl", "<cmd>ChatGPTRun code_readability_analysis<CR>", desc = "Code Readability Analysis", mode = { "n", "v" } },
+  },
   config = function()
     require("chatgpt").setup()
   end
--- lua/cwebster/plugins/which-key.lua
+++ lua/cwebster/plugins/which-key.lua
@@ -1,60 +1,61 @@
 return {
-	{
-		"folke/which-key.nvim",
-		opts = {
-			operators = { gc = "Comments", ys = "Surround", yS = "Surround" },
-			window = { winblend = 30 },
-		},
-		config = function(plugin, opts)
-			local util = require("cwebster.utils")
-			local wk = require("which-key")
-			wk.setup(plugin.opts)
+  {
+    "folke/which-key.nvim",
+    opts = {
+      operators = { gc = "Comments", ys = "Surround", yS = "Surround" },
+      window = { winblend = 30 },
+    },
+    config = function(plugin, opts)
+      local util = require("cwebster.utils")
+      local wk = require("which-key")
+      wk.setup(plugin.opts)
 
-			local leader_map = {}
-			leader_map.g = { name = "+git" }
-			leader_map.g.h = { name = "+hunks" }
-			leader_map.b = { name = "+buffer" }
-			leader_map.h = { name = "+help" }
-			leader_map.h.p = { name = "+packer" }
-			leader_map.f = { name = "+find" }
-			leader_map.s = { name = "+search" }
-			leader_map.x = { name = "+trouble" }
-			leader_map.t = { name = "+testing" }
-			leader_map.d = { name = "+debug" }
-			local normal_map = {
-				["g"] = {
-					name = "+goto",
-					["z"] = {
-						name = "+zettle",
-						n = { "New" },
-						z = { "Find/create" },
-						Z = { "Insert ID" },
-						b = { "Backlinks" },
-						B = { "Id of backlink" },
-						t = { "Find/insert tags" },
-						s = { "Start server" },
-						["]"] = { "Next Link" },
-						["["] = { "Prev Link" },
-					},
-				},
-			}
+      local leader_map = {}
+      leader_map.g = { name = "+git" }
+      leader_map.g.h = { name = "+hunks" }
+      leader_map.b = { name = "+buffer" }
+      leader_map.h = { name = "+help" }
+      leader_map.h.p = { name = "+packer" }
+      leader_map.f = { name = "+find" }
+      leader_map.s = { name = "+search" }
+      leader_map.x = { name = "+trouble" }
+      leader_map.t = { name = "+testing" }
+      leader_map.d = { name = "+debug" }
+      leader_map.c.g = { name = "+Chat GPT" }
+      local normal_map = {
+        ["g"] = {
+          name = "+goto",
+          ["z"] = {
+            name = "+zettle",
+            n = { "New" },
+            z = { "Find/create" },
+            Z = { "Insert ID" },
+            b = { "Backlinks" },
+            B = { "Id of backlink" },
+            t = { "Find/insert tags" },
+            s = { "Start server" },
+            ["]"] = { "Next Link" },
+            ["["] = { "Prev Link" },
+          },
+        },
+      }
 
-			wk.register(leader_map, { prefix = "<leader>" })
-			wk.register(normal_map)
+      wk.register(leader_map, { prefix = "<leader>" })
+      wk.register(normal_map)
 
-			util.on_attach(function(_, bufnr)
-				local keymap = {}
-				keymap.c = { name = "+Code Actions" }
-				keymap.c.l = { name = "+LSP Actions" }
-				keymap.x = { name = "+LSP Diagnostics" }
-				local visual_keymap = {}
-				visual_keymap.c = { name = "+Code Actions" }
-				local goto_keymap = {}
-				goto_keymap.g = { name = "+goto" }
-				wk.register(keymap, { buffer = bufnr, prefix = "<leader>" })
-				wk.register(visual_keymap, { buffer = bufnr, prefix = "<leader>", mode = "v" })
-				wk.register(goto_keymap, { buffer = bufnr, prefix = "g" })
-			end)
-		end,
-	},
+      util.on_attach(function(_, bufnr)
+        local keymap = {}
+        keymap.c = { name = "+Code Actions" }
+        keymap.c.l = { name = "+LSP Actions" }
+        keymap.x = { name = "+LSP Diagnostics" }
+        local visual_keymap = {}
+        visual_keymap.c = { name = "+Code Actions" }
+        local goto_keymap = {}
+        goto_keymap.g = { name = "+goto" }
+        wk.register(keymap, { buffer = bufnr, prefix = "<leader>" })
+        wk.register(visual_keymap, { buffer = bufnr, prefix = "<leader>", mode = "v" })
+        wk.register(goto_keymap, { buffer = bufnr, prefix = "g" })
+      end)
+    end,
+  },
 }
