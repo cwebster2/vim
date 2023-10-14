e130ba02baa7358c341784b4e3021101e7ea1310
Casey Webster <casey.webster@gmail.com>
2023-10-14 12:17:55 -0500

    moved conform out of lsp setup

--- lua/cwebster/plugins/lsp/init.lua
+++ lua/cwebster/plugins/lsp/init.lua
@@ -154,34 +154,6 @@ return {
   },
 
   {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        -- in sublist runs the first available choice
-        javascript = { { "prettierd", "prettier" } },
-        javascriptreact = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        typescriptreact = { { "prettierd", "prettier" } },
-        vue = { { "prettierd", "prettier" } },
-        css = { { "prettierd", "prettier" } },
-        scss = { { "prettierd", "prettier" } },
-        less = { { "prettierd", "prettier" } },
-        html = { { "prettierd", "prettier" } },
-        json = { { "prettierd", "prettier" } },
-        yaml = { { "prettierd", "prettier" } },
-        markdown = { { "prettierd", "prettier" } },
-        graphql = { { "prettierd", "prettier" } },
-        svelte = { { "prettierd", "prettier" } },
-        toml = { { "prettierd", "prettier" } },
-        -- multiple top level all run
-        go = { "gofmt", "goimports" },
-        rust = { "rustfmt" },
-      },
-    },
-  },
-
-  {
     "jose-elias-alvarez/null-ls.nvim",
     dependencies = {
       { "mason.nvim" },
