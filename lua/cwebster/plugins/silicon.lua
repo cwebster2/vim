735d6ba8e80b20425b052c69970d52381e6cd8e7
Casey Webster <cwebster@glgroup.com>
2024-04-11 12:50:37 -0500

    Switching the silicon module

--- lua/cwebster/plugins/silicon.lua
+++ lua/cwebster/plugins/silicon.lua
@@ -1,12 +1,23 @@
 return {
-  "krivahtoo/silicon.nvim",
-  build = "./install.sh build",
-  cmd = "Silicon",
-  config = function()
-    require("silicon").setup({
-      watermark = {
-        text = "@cwebster2",
-      },
-    })
-  end,
+  {
+    "krivahtoo/silicon.nvim",
+    enabled = false,
+    build = "./install.sh build",
+    cmd = "Silicon",
+    config = function()
+      require("silicon").setup({
+        watermark = {
+          text = "@cwebster2",
+        },
+      })
+    end,
+  },
+  {
+    "michaelrommel/nvim-silicon",
+    cmd = "Silicon",
+    opts = {
+      font = "Hack Nerd Font Mono",
+      to_clipboard = true,
+    },
+  },
 }
