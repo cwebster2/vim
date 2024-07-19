a5388b5dd3425129813ef8e4c920ce76dafd2757
Casey Webster <cwebster@glgroup.com>
2024-07-19 15:51:08 -0500

    chore: update statuscolumn fold look

--- lua/cwebster/plugins/statuscol.lua
+++ lua/cwebster/plugins/statuscol.lua
@@ -9,9 +9,8 @@ return {
       return {
         segments = {
           -- { text = { "%C" }, click = "v:lua.ScFa" },
-          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
           {
-            sign = { name = { "neotest", "Dap" }, maxwidth = 1, auto = false },
+            sign = { name = { "neotest", "Dap" }, maxwidth = 1, auto = true },
             click = "v:lua.ScSa",
           },
           {
@@ -27,6 +26,7 @@ return {
             sign = { namespace = { "gitsign*" }, maxwidth = 1, colwidth = 1, auto = false },
             click = "v:lua.ScSa",
           },
+          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
           -- { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
           -- {
           --   sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
