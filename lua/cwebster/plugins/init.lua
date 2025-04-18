5ac04bb78d7fb7767d7d922061698c00b6226489
Casey Webster <casey.webster@gmail.com>
2025-04-18 12:10:18 -0500

    chore: enable some LSPs

--- lua/cwebster/plugins/init.lua
+++ lua/cwebster/plugins/init.lua
@@ -15,7 +15,7 @@ return {
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          comments = { italic = true }, -- Disable italics in comments
         },
       }
 
