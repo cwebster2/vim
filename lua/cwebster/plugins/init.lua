c2fcc17706056c581c82f499778f3fa75d415f04
Casey Webster <casey.webster@gmail.com>
2025-04-18 22:00:34 -0500

    feat(theme): add Catppuccin colorscheme support
    Added the Catppuccin colorscheme plugin with configuration for the
    'mocha' flavor, italic comments, and integrations for gitsigns,
    treesitter, and mini. This provides an alternative to the existing
    Tokyonight theme, allowing users to switch between themes easily.
    
    Updated lazy-lock.json to include the Catppuccin plugin commit hash.

--- lua/cwebster/plugins/init.lua
+++ lua/cwebster/plugins/init.lua
@@ -4,29 +4,44 @@ return {
     'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
         transparent = true,
         styles = {
-          comments = { italic = true }, -- Disable italics in comments
+          comments = { italic = true },
         },
       }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
+  {
+    'catppuccin/nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    name = "catppuccin",
+    config = function()
+      require('catppuccin').setup {
+        --transparent_background = true,
+        flavours = { 'mocha' },
+        styles = {
+          comments = { "italic" },
+        },
+        integrations = {
+          gitsigns = true,
+          treesitter = true,
+          mini = {
+            enabled = true,
+          },
+        },
+      }
+
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
