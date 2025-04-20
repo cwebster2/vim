29e3970927d0ec391048c1dd4156d4b3d4d0eb11
Casey Webster <casey.webster@gmail.com>
2025-04-20 18:37:27 -0500

    feat(snacks): add key mappings and enhance plugin options
    - Introduced new key mappings for various Snacks.nvim functionalities:
      - Toggle Zen Mode, Zoom, Scratch Buffer, and Terminal.
      - Access Notification History and Neovim News.
      - Delete Buffer and select Scratch Buffer.
    - Enhanced plugin options:
      - Enabled bigfile and quickfile support.
      - Configured notifier with timeout and enabled Zen mode.
    - Improved status column configuration for better usability.

--- lua/cwebster/plugins/snacks.lua
+++ lua/cwebster/plugins/snacks.lua
@@ -3,8 +3,43 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
+    keys = {
+      { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+      { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+      { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+      { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+      { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+      { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+      { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+      { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+      {
+        "<leader>N",
+        desc = "Neovim News",
+        function()
+          Snacks.win({
+            file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+            width = 0.6,
+            height = 0.6,
+            wo = {
+              spell = false,
+              wrap = false,
+              signcolumn = "yes",
+              statuscolumn = " ",
+              conceallevel = 3,
+            },
+          })
+        end,
+      }
+    },
     opts = {
-      notifier = {},
+      bigfile = { enabled = true },
+      quickfile = { enabled = true },
+      terminal = {},
+      notifier = {
+        enabled = true,
+        timeout = 3000,
+      },
+      zen = { enabled = true },
       statuscolumn = {
         left = { "mark", "sign" },
         right = { "fold", "git" },
