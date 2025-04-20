f769f3497c33aebdcf58b364683a9ad3cde721c8
Casey Webster <casey.webster@gmail.com>
2025-04-19 20:20:11 -0500

    feat(neogit): add telescope integration
    Added "nvim-telescope/telescope.nvim" as a dependency for Neogit and
    enabled its integration. This enhances the functionality of Neogit by
    leveraging Telescope for improved navigation and search capabilities.

--- lua/cwebster/plugins/neogit.lua
+++ lua/cwebster/plugins/neogit.lua
@@ -1,24 +1,20 @@
 return {
   {
     "NeogitOrg/neogit",
-    dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
     -- branch = "nightly",
     event = "VeryLazy",
     keys = {
       { "<leader>gg", function() require("neogit").open() end, desc = "Open Neogit" },
     },
     opts = {
-      disable_signs = false,
-      disable_context_highlighting = false,
-      disable_commit_confirmation = true,
-      signs = {
-        -- { CLOSED, OPENED }
-        section = { ">", "v" },
-        item = { ">", "v" },
-        hunk = { "", "" },
-      },
       integrations = {
         diffview = true,
+        telescope = true,
       },
     },
   },
