c56fde4776f944cdfa5f4aff86d34636af5de1a0
Casey Webster <cwebster@glgroup.com>
2025-04-21 11:40:29 -0500

    feat(plugins): update plugin versions and refactor configs
    Updated plugin versions in `lazy-lock.json` to include the latest commits
    for improved functionality and bug fixes. Refactored plugin configurations
    in `init.lua` to use consistent string quoting and added new options for
    better customization. Extracted `mini.nvim` configuration to a separate
    file for modularity and maintainability.

--- lua/cwebster/plugins/init.lua
+++ lua/cwebster/plugins/init.lua
@@ -1,32 +1,32 @@
 return {
   {
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+    -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+    "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
   },
 
   {
-    'folke/tokyonight.nvim',
+    "folke/tokyonight.nvim",
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      require('tokyonight').setup {
+      require("tokyonight").setup({
         -- transparent = true,
         styles = {
           comments = { italic = true },
         },
-      }
+      })
 
       -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
   {
-    'catppuccin/nvim',
+    "catppuccin/nvim",
     priority = 1000, -- Make sure to load this before all the other start plugins.
     name = "catppuccin",
     config = function()
-      require('catppuccin').setup {
-        --transparent_background = true,
-        flavours = { 'mocha' },
+      require("catppuccin").setup({
+        transparent_background = true,
+        flavours = { "mocha" },
         styles = {
           comments = { "italic" },
         },
@@ -36,58 +36,43 @@ return {
           mini = {
             enabled = true,
           },
+          blink_cmp = true,
+          dap = true,
+          dap_ui = true,
+          diffview = true,
+          dropbar = {
+            enabled = true,
+            color_mode = true,
+          },
+          indent_blankline = {
+            enabled = true,
+          },
+          native_lsp = {
+            enabled = true,
+          },
+          neogit = true,
+          mason = true,
+          snacks = true,
+          telescope = true,
+          which_key = true,
         },
-      }
+      })
 
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-       return '%2l:%-2v'
-      end
-
-      require("mini.trailspace").setup()
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
+  {
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = { signs = false },
   },
+
   { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs", -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = "all",
@@ -98,9 +83,9 @@ return {
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        additional_vim_regex_highlighting = { "ruby" },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { "ruby" } },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
@@ -108,5 +93,5 @@ return {
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  }
+  },
 }
