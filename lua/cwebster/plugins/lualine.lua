fbe87150ab9673bb9ba8bac22cd331906ec4433c
Casey Webster <cwebster@glgroup.com>
2025-04-21 12:44:09 -0500

    feat(lualine): add lualine.nvim for enhanced statusline
    Added lualine.nvim plugin with configuration for a customizable and
    informative statusline. This replaces the previous MiniStatusline
    setup, which has been commented out for potential future reference.
    
    - Configured lualine sections for mode, branch, diagnostics, filename,
      encoding, file format, file type, progress, and location.
    - Added dependency on nvim-web-devicons for better visuals.

---
+++ lua/cwebster/plugins/lualine.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      globalstatus = true,
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    },
+  },
+}
