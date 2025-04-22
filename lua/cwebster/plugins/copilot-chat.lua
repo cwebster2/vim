900ccfecf82229580528c99d63bc90e43f963155
Casey Webster <cwebster@glgroup.com>
2025-04-22 15:40:47 -0500

    Try things to get c-y working

--- lua/cwebster/plugins/copilot-chat.lua
+++ lua/cwebster/plugins/copilot-chat.lua
@@ -1,13 +1,19 @@
-  return {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "main",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    opts = {
-      -- debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  }
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "main",
+  dependencies = {
+    { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+    { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+  },
+  opts = {
+    -- debug = true, -- Enable debugging
+    -- See Configuration section for rest
+    -- mappings = {
+    --   accept_diff = {
+    --     normal = "<M-y>",
+    --     insert = "<M-y>",
+    --   },
+    -- },
+  },
+  -- See Commands section for default commands if you want to lazy load on them
+}
