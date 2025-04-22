bd02d3c906298d99ad18481f7e88e1808b4a6f57
Casey Webster <cwebster@glgroup.com>
2025-04-22 16:52:44 -0500

    Add Neorg

--- lua/cwebster/plugins/neorg.lua
+++ lua/cwebster/plugins/neorg.lua
@@ -2,10 +2,7 @@ return {
   {
     "nvim-neorg/neorg",
     lazy = false,
-    -- ft = "norg",
-    -- cmd = "Neorg",
     dependencies = {
-      "luarocks.nvim",
       "nvim-lua/plenary.nvim",
     },
     config = function()
@@ -13,11 +10,11 @@ return {
         load = {
           ["core.defaults"] = {}, -- Loads default behaviour
           ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.completion"] = {
-            config = {
-              engine = "nvim-cmp",
-            },
-          },
+          -- ["core.completion"] = {
+          --   config = {
+          --     engine = "nvim-cmp",
+          --   },
+          -- },
           ["core.dirman"] = { -- Manages Neorg workspaces
             config = {
               workspaces = {
