e6e249d037b062bb101e40efa7e3ccdac1bcfde8
Casey Webster <cwebster@glgroup.com>
2025-04-23 19:41:53 -0500

    Adding zellij-nav plugin

--- lua/cwebster/plugins/tmux.lua
+++ lua/cwebster/plugins/tmux.lua
@@ -1,18 +1,43 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-    "TmuxNavigatorProcessList",
+  {
+    "christoomey/vim-tmux-navigator",
+    enabled = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<M-m>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<M-n>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<M-e>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<M-i>", "<cmd>TmuxNavigateRight<cr>" },
+      -- { '<C-\\>', '<cmd>TmuxNavigatePrevious<cr>' },
+    },
   },
-  keys = {
-    { "<M-m>", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<M-n>", "<cmd>TmuxNavigateDown<cr>" },
-    { "<M-e>", "<cmd>TmuxNavigateUp<cr>" },
-    { "<M-i>", "<cmd>TmuxNavigateRight<cr>" },
-    -- { '<C-\\>', '<cmd>TmuxNavigatePrevious<cr>' },
+  {
+    {
+      "swaits/zellij-nav.nvim",
+      lazy = true,
+      event = "VeryLazy",
+      keys = {
+        {
+          "<M-m>",
+          "<cmd>ZellijNavigateLeftTab<cr>",
+          { silent = true, desc = "navigate left or tab" },
+        },
+        { "<M-n>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+        { "<M-e>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+        {
+          "<M-i>",
+          "<cmd>ZellijNavigateRightTab<cr>",
+          { silent = true, desc = "navigate right or tab" },
+        },
+      },
+      opts = {},
+    },
   },
 }
