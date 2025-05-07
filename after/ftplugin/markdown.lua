2d8618ee5b8a265a890635b9f960c920edfd4240
Casey Webster <cwebster@glgroup.com>
2025-05-07 16:50:35 -0500

    feat(markdown, plugins): enhance conceallevel and add dadbod
    - Updated `markdown.lua` to set `conceallevel` to 2 for better readability.
    - Commented out unused conceallevel configuration for the indentLine plugin.
    - Added `vim-dadbod`, `vim-dadbod-ui`, and `vim-dadbod-completion` plugins
      for database management and SQL completion.
    - Configured `vim-dadbod-ui` with commands and initialized with Nerd Fonts.
    
    These changes improve markdown editing and add database management tools.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,3 @@
-
 vim.opt_local.foldmethod = "expr"
 vim.opt_local.textwidth = 80
 vim.opt_local.colorcolumn = "81"
@@ -6,15 +5,14 @@ vim.opt_local.wrapmargin = 0
 vim.opt_local.spell = true
 
 -- vim.opt_local.formatoptions = vim.opt.formatoptions -"t"
--- vim.opt_local.conceallevel = 0
-vim.cmd[[
-  " Disable hidden char due to indentLine plugin
-  let g:indentLine_concealcursor = 'invc'
-  let g:indentLine_conceallevel = 2
-  if &readonly || &buftype =~ 'nofile'
-    setlocal conceallevel=2
-  else
-    setlocal conceallevel=0
-  endif
-]]
-
+vim.opt_local.conceallevel = 2
+-- vim.cmd[[
+--   " Disable hidden char due to indentLine plugin
+--   let g:indentLine_concealcursor = 'invc'
+--   let g:indentLine_conceallevel = 2
+--   if &readonly || &buftype =~ 'nofile'
+--     setlocal conceallevel=2
+--   else
+--     setlocal conceallevel=0
+--   endif
+-- ]]
