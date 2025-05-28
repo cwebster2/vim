137a343ba029e237e993683aab364e5ca5d6a0ba
Casey Webster <cwebster@glgroup.com>
2025-05-28 09:27:00 -0500

    fix: use unnamed clipboard of plus doesn't exist

--- lua/cwebster/options.lua
+++ lua/cwebster/options.lua
@@ -1,21 +1,25 @@
 vim.opt.number = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.showmode = false
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+  if vim.fn.has("unnamedplus") then
+    vim.opt.clipboard = "unnamedplus"
+  else
+    vim.opt.clipboard = "unnamed"
+  end
 end)
 vim.opt.breakindent = true
 vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.confirm = true
